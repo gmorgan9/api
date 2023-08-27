22d29084d1211e95145796dd7286bba6b0f2371a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-26 23:40:23 -0600

    update

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ async function startServer() {
             req.session.user = result.rows[0];
             console.log('Login successful, redirecting...');
             // Send a success response for the POST request
-            return(url_for('https://app-aarc.morganserver.com/dashboard/'));
+            return res.redirect(302, 'https://app-aarc.morganserver.com/dashboard/');
           } else {
             return res.status(401).json({ success: false, message: 'Invalid password' });
           }
