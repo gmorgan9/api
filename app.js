36c0d1508f6f8061df4f3defb903199f98e66432
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-26 21:26:10 -0600

    update

--- app.js
+++ app.js
@@ -6,11 +6,10 @@ const app = express();
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // For local development; remove in production.
+  }
 });
 
 pool.connect()
@@ -23,7 +22,7 @@ pool.connect()
   });
 
 function startServer() {
-  const port = 3000;
+  const port = process.env.PORT || 3000;
 
   app.get('/', (req, res) => {
     res.send('Hello World!');
