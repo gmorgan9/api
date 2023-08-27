4ab911604c01ed3a3f6bee18d92609a421735737
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-26 21:57:15 -0600

    update

--- app.js
+++ app.js
@@ -49,41 +49,23 @@ function startServer() {
 
   // Endpoint to handle user login
   app.post('/api/login', async (req, res) => {
-    const { work_email, password } = req.body;
+  const { work_email, password } = req.body;
 
-    try {
-      // Query the database to check user credentials
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM users WHERE work_email = $1', [work_email]);
+  // Calculate the MD5 hash of the provided password
+  const hashedPassword = md5(password);
 
-      if (result.rows.length === 1) {
-        // Get the hashed password from the database
-        const hashedPasswordFromDB = result.rows[0].password;
+  try {
+    // Query the database to check user credentials
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM users WHERE work_email = $1', [work_email]);
 
-        // Calculate the MD5 hash of the provided password
-        const hashedPassword = md5(password);
+    // Rest of your code...
+  } catch (err) {
+    console.error('Error during login', err);
+    res.status(500).json({ success: false, message: 'Internal Server Error' });
+  }
+});
 
-        if (hashedPassword === hashedPasswordFromDB) {
-          // User is authenticated; store user data in the session
-          req.session.user = result.rows[0];
-          res.json({ success: true, message: 'Login successful' });
-        } else {
-          res.status(401).json({ success: false, message: 'Invalid credentials' });
-        }
-      } else {
-        res.status(401).json({ success: false, message: 'Invalid credentials-outside' });
-        console.log('work_email:', work_email);
-        console.log('hashedPassword:', hashedPassword);
-
-      }
-
-      // Release the database connection
-      client.release();
-    } catch (err) {
-      console.error('Error during login', err);
-      res.status(500).json({ success: false, message: 'Internal Server Error' });
-    }
-  });
 
   // Endpoint to check if the user is authenticated
   app.get('/api/check-auth', (req, res) => {
