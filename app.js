d6ad8b66b41400a20fd738179af4900cd7d2d74b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-26 21:24:11 -0600

    update

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
 const express = require('express');
+const { Pool } = require('pg');
 const app = express();
-const host = '100.118.102.62';
-const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Load environment variables from .env
+require('dotenv').config();
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+pool.connect()
+  .then(() => {
+    console.log('Connected to PostgreSQL database');
+    startServer();
+  })
+  .catch((err) => {
+    console.error('Error connecting to PostgreSQL database', err);
+  });
 
-app.get('/api/data', (req, res) => {
-  const data = { message: 'This is your API data!' };
-  res.json(data);
-});
+function startServer() {
+  const port = 3000;
 
-app.use((req, res, next) => {
-  console.log(`Received ${req.method} request to ${req.url}`);
-  next();
-});
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+
+  app.get('/api/data', (req, res) => {
+    const data = { message: 'This is your API data!' };
+    res.json(data);
+  });
+
+  app.use((req, res, next) => {
+    console.log(`Received ${req.method} request to ${req.url}`);
+    next();
+  });
+}
