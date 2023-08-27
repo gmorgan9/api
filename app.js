b8dd4e86e4a67e0632247f66458482f9a13d4dd6
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-08-27 00:13:50 -0600

    update

--- app.js
+++ app.js
@@ -31,35 +31,59 @@ pool.connect()
         console.error('Error connecting to PostgreSQL database', err);
     });
 
-app.post('/api/login', async (req, res) => {
-    const { work_email, password } = req.body;
+    app.post('/api/login', async (req, res) => {
+      const { work_email, password } = req.body;
+  
+      try {
+          const client = await pool.connect();
+          const result = await client.query('SELECT * FROM users WHERE work_email = $1', [work_email]);
+  
+          if (result.rows.length === 1) {
+              const user = result.rows[0];
+              const hashedPasswordFromDB = user.password;
+              const passwordMatch = await bcrypt.compare(password, hashedPasswordFromDB);
+  
+              if (passwordMatch) {
+                  // Update the logged_in status for the user to 1
+                  await client.query('UPDATE users SET logged_in = 1 WHERE id = $1', [user.id]);
+  
+                  console.log('Login successful for user:', work_email);
+                  return res.status(200).json({ success: true, message: 'Login successful-api' });
+              } else {
+                  console.log('Invalid password for user:', work_email);
+                  return res.status(401).json({ success: false, message: 'Invalid password' });
+              }
+          } else {
+              return res.status(401).json({ success: false, message: 'User not found' });
+          }
+  
+          client.release();
+      } catch (err) {
+          console.error('Error during login', err);
+          return res.status(500).json({ success: false, message: 'Internal Server Error' });
+      }
+  });
+
+
+  app.post('/api/logout', async (req, res) => {
+    // Identify the user (e.g., from the authentication token)
+    const userId = req.user.id; // Adjust this based on your authentication method
 
     try {
         const client = await pool.connect();
-        const result = await client.query('SELECT * FROM users WHERE work_email = $1', [work_email]);
 
-        if (result.rows.length === 1) {
-            const hashedPasswordFromDB = result.rows[0].password;
-            const passwordMatch = await bcrypt.compare(password, hashedPasswordFromDB);
+        // Update the logged_in status for the user to 0
+        await client.query('UPDATE users SET logged_in = 0 WHERE id = $1', [userId]);
 
-            if (passwordMatch) {
-              // Login successful
-              console.log('Login successful for user:', work_email);
-              return res.status(200).json({ success: true, message: 'Login successful-api' });
-          } else {
-              console.log('Invalid password for user:', work_email);
-              return res.status(401).json({ success: false, message: 'Invalid password' });
-          }
-        } else {
-            return res.status(401).json({ success: false, message: 'User not found' });
-        }
-
-        client.release();
+        console.log('Logout successful for user:', userId);
+        return res.status(200).json({ success: true, message: 'Logout successful' });
     } catch (err) {
-      console.error('Error during login', err);
-      return res.status(500).json({ success: false, message: 'Internal Server Error' });
-  }
+        console.error('Error during logout', err);
+        return res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
 });
+
+  
 
 const port = 3000;
 app.listen(port, () => {
